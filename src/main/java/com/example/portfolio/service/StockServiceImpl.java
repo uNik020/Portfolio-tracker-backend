5061d3f801f141c43a562dc0c22193e1c91385a7
uNik020 <shekharnikhil020@gmail.com>
2025-01-22 18:23:43 +0530

    api key changed

--- src/main/java/com/example/portfolio/service/StockServiceImpl.java
+++ src/main/java/com/example/portfolio/service/StockServiceImpl.java
@@ -62,8 +62,8 @@ public class StockServiceImpl implements StockService {
     private BigDecimal fetchRealTimePrice(String ticker) {
         //String apiKey = "IOW2CU52KSZ5IUA9"; // Replace with your actual API key
 	//String apiKey = "NJ9DX4CNHL7AZ5Z4";
-	//String apiKey = "30CO7P3MM54Y0GK3";
-	String apiKey = "A7ABH0K2MWHX9I78";
+	String apiKey = "30CO7P3MM54Y0GK3";
+	//String apiKey = "A7ABH0K2MWHX9I78";
 	//String apiKey = "TNCP2RGNPOAGN9ST";        
 
 
